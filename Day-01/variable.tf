9e61b778ec789a4fca9c5363e811c67f7254728e
Anurag-R1974 <sanuragramagiri@gmail.com>
2025-04-27 00:40:22 +0530

    Terraform Day-1

---
+++ Day-01/variable.tf
@@ -0,0 +1,11 @@
+variable "ami_id" {
+  description = "variable creation for ami id"
+  type = string
+  default = "ami-0e449927258d45bc4"
+}
+variable "instance_type" {
+  description = "variable cration for ec2 type"
+  type = string
+  default = "t2.micro"
+
+}
